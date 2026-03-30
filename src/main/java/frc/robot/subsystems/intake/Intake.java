f75c65f01f151f568d96dc9e311edd7bc24076e8
SpaceCattis1 <spacecattis@gmail.com>
2026-03-30 12:18:39 -0700

    intake abstraction - 3/30 12:18 PM

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,4 +2,18 @@ package frc.robot.subsystems.intake;
 
 public class Intake {
     
+    // Subsystem abstraction.
+    private final IntakeIO io;
+    private final IntakeInputs inputs;
+
+    // State system.
+    public enum State {
+        FORWARD,
+        REVERSE,
+        STOPPED
+    }
+    /** Subsystem state. */
+    public final StateManager<State> manager = new StateManager<State>(
+        getName(), State.READING
+    );
 }
